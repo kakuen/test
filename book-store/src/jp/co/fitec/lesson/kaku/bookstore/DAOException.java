1191fc0fe2a2ae2678c1d571e18e9e5ac91ec031
kaku en <jxguoyuan@gmail.com>
2014-07-07 10:41:19 +0900

    Commit

--- book-store/src/jp/co/fitec/lesson/kaku/bookstore/DAOException.java
+++
@@ -1,8 +0,0 @@
-package jp.co.fitec.lesson.kaku.bookstore;
-
-public class DAOException extends Exception {
-	
-	public DAOException(Exception e) {
-		super("An exception occured in the DAO layer:", e);
-	}
-}
